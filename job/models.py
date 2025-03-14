261cc6e1becbe1c383ee374444cb21f8bf5e8948
nadhirbahri <bahri.mohamed.nadhir@outlook.com>
2025-03-14 22:53:44 +0100

    add jobs viws and url

--- job/models.py
+++ job/models.py
@@ -12,8 +12,16 @@ JOB_TYPE=(('Full Time','Full Time'),
           ('Part Time','Part Time'),
           )
 
+class category(models.Model):
+    name= models.CharField(max_length=30)
+    def __str__(self):
+    
+          return self.name
+
+
 
 # Create your models here.
+
 class job(models.Model): #table
     title = models.CharField(max_length=100) #column
     job_type = models.CharField(max_length=12 , choices=JOB_TYPE)
@@ -22,6 +30,7 @@ class job(models.Model): #table
     Vacancy = models.IntegerField(default=1)
     Salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
+    category = models.ForeignKey('category',on_delete=models.CASCADE)
     def __str__(self):
     
           return self.title 
